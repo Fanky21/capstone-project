e47b2234858c96564992900b80b888f9361066b3
Noval ian <andrianau26@gmail.com>
2025-11-04 02:44:39 +0800

    berita & invest panel

---
+++ Assets/Script/Invest/InvestPanel.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+
+public class InvestPanel : MonoBehaviour
+{
+    [Header("Main Panels")]
+    [SerializeField] private GameObject investPanel;
+    [SerializeField] private GameObject investButton;
+
+    [Header("Sub Panels")]
+    [SerializeField] private GameObject cryptoPanel;
+    [SerializeField] private GameObject sahamPanel;
+    [Header("Button Berita")]
+    [SerializeField] private GameObject beritaButton;
+    [SerializeField] private GameObject pauseButton;
+
+    private bool isInvestOpen = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && isInvestOpen)
+        {
+            ExitPanel();
+        }
+    }
+
+    public void OpenInvestPanel()
+    {
+        PlayButtonSound();
+        isInvestOpen = true;
+
+        investPanel.SetActive(true);
+        investButton.SetActive(false);
+        beritaButton.SetActive(false);
+        pauseButton.SetActive(false);
+
+        cryptoPanel.SetActive(true);
+        sahamPanel.SetActive(false);
+
+        Time.timeScale = 0f; // pause game
+    }
+
+    public void OpenCryptoPanel()
+    {
+        PlayButtonSound();
+        cryptoPanel.SetActive(true);
+        sahamPanel.SetActive(false);
+    }
+
+    public void OpenSahamPanel()
+    {
+        PlayButtonSound();
+        sahamPanel.SetActive(true);
+        cryptoPanel.SetActive(false);
+    }
+
+    public void ExitPanel()
+    {
+        PlayButtonSound();
+        isInvestOpen = false;
+
+        investPanel.SetActive(false);
+        investButton.SetActive(true);
+        beritaButton.SetActive(true);
+        pauseButton.SetActive(true);
+
+        cryptoPanel.SetActive(false);
+        sahamPanel.SetActive(false);
+
+        Time.timeScale = 1f; // resume game
+    }
+
+    private void PlayButtonSound()
+    {
+        if (SoundManager.Instance != null)
+        {
+            SoundManager.Instance.PlaySound2D("Button");
+        }
+    }
+}
